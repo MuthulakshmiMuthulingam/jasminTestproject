7167427eaddb85c9e3e053fa30b183d98a92ada1
Muthulakshmi M <muthulakshmi.m@nilaappsindia.com>
2020-05-20 17:47:31 +0530

    update file

--- spec/Test1Spec.js
+++ spec/Test1Spec.js
@@ -13,7 +13,7 @@ describe('PasswordController', function() {
       it('sets the strength to "strong" if the password length is >8 chars', function() {
         var $scope = $rootScope.$new();
         var controller = $controller('PasswordController', { $scope: $scope });
-        $scope.password = 'longerthaneightchars';
+        $scope.password = 'mm';
         $scope.grade();
         expect($scope.strength).toEqual('strong');
       });
